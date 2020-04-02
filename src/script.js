f7798982566cfdf486fe64eb881cd4fd417d6b9e
ALEX\alex <lyssenko.alex@gmail.com>
2020-04-03 01:43:01 +0600

    refactor: some style

--- src/script.js
+++ src/script.js
@@ -6,28 +6,22 @@ import {CLASS_SIZE} from "./constants.js";
 init('eng');
 
 function init(lang) {
-    console.log('stater')
 
     let el = document.querySelector(".keyboard");
     let row = document.createElement('div');
     row.classList.add('row');
-    ALPHABET.filter((x) =>{
-       return true
-    }).forEach((y) => {
+    ALPHABET.forEach((y) => {
         let button = document.createElement('button');
         button.type = 'button';
         button.classList.add(CLASS_SIZE[y.size].CLASS);
         let span = document.createElement('span');
-        span.classList.add('sm')
+        span.classList.add('sm');
         let textNode = document.createTextNode(y.eng);
         span.append(textNode);
-
         button.append(span);
         row.append(button);
         el.append(row);
     })
-
-
 
 }
 
